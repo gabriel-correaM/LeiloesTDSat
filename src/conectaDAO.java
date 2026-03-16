d3f15cad08cc02f67886171dc5f57894a91c30fb
Gabriel Macedo <eusouhavanprr@gmail.com>
2026-03-16 16:09:10 -0300

    Conexão do banco de dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,33 +1,30 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
+
     public Connection connectDB(){
+
         Connection conn = null;
-        
+
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+
+            String url = "jdbc:mysql://localhost:3306/leiloes_tdsat";
+            String user = "root";
+            String password = "root";
+
+            conn = DriverManager.getConnection(url, user, password);
+
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!");
+
+        } catch (SQLException erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro ao conectar: " + erro.getMessage());
+
         }
+
         return conn;
     }
-    
 }
